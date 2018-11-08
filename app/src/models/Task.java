4bd90c1decf133af967398f0e37049a1e0858593
Heldoe <kadirciftciict@gmail.com>
2018-11-08 13:29:54 +0100

    createdModels

---
+++ app/src/models/Task.java
@@ -0,0 +1,10 @@
+public class Task {
+    private String id;
+    private String title;
+    private String description;
+    private String title;
+    private String title;
+    private int quantity;
+
+
+}
