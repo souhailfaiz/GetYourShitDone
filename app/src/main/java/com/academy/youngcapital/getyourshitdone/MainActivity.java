1dd8717a5861551eb34cc9747ed7c142e65c6f7a
Heldoe <kadirciftciict@gmail.com>
2018-11-09 12:18:23 +0100

    x

--- app/src/main/java/com/academy/youngcapital/getyourshitdone/MainActivity.java
+++ app/src/main/java/com/academy/youngcapital/getyourshitdone/MainActivity.java
@@ -30,9 +30,36 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        LinearLayout layout = new LinearLayout(this);
         switch (item.getItemId()) {
             case R.id.action_add_task:
-                LinearLayout layout = new LinearLayout(this);
+
+                layout.setOrientation(LinearLayout.VERTICAL);
+                final EditText taskEditText11 = new EditText(this);
+                final EditText taskEditText21 = new EditText(this);
+                layout.addView(taskEditText11);
+                taskEditText11.setHint("Task title");;
+                taskEditText21.setHint("Task Description");;
+                layout.addView(taskEditText21);
+                AlertDialog dialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new Task")
+                        .setView(layout)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String categoryName = String.valueOf(taskEditText11.getText());
+                                String categoryColor = String.valueOf(taskEditText21.getText());
+
+                                Category category = new Category(categoryName, categoryColor);
+                                Log.d(TAG, "Category to add: " + category.getColor()+ category.getTitle());
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                dialog.show();
+                return true;
+
+            case R.id.action_add_category:
                 layout.setOrientation(LinearLayout.VERTICAL);
                 final EditText taskEditText1 = new EditText(this);
                 final EditText taskEditText2 = new EditText(this);
@@ -40,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 taskEditText1.setHint("Category name");;
                 taskEditText2.setHint("Category Color");;
                 layout.addView(taskEditText2);
-                AlertDialog dialog = new AlertDialog.Builder(this)
+                AlertDialog dialog1 = new AlertDialog.Builder(this)
                         .setTitle("Add a new Category")
                         .setView(layout)
                         .setPositiveButton("Add", new DialogInterface.OnClickListener() {
@@ -55,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                         })
                         .setNegativeButton("Cancel", null)
                         .create();
-                dialog.show();
+                dialog1.show();
                 return true;
 
             default:
