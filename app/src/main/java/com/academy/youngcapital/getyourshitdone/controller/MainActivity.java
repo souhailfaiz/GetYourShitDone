da6a2e81f7dbbea0f619f9cb1d860a97edcd844e
Amin <amin@Amins-MacBook-Pro.local>
2018-11-12 10:55:45 +0100

    navi

--- app/src/main/java/com/academy/youngcapital/getyourshitdone/controller/MainActivity.java
+++ app/src/main/java/com/academy/youngcapital/getyourshitdone/controller/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -25,16 +27,29 @@ import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
 
-    private static final String TAG = "MainActivity";
+    private static final
+    String TAG = "MainActivity";
 
     // data/tasks.class
     private Tasks dataTasks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerId);
+
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         dataTasks = new Tasks();
 
@@ -155,5 +170,14 @@ public class MainActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+
+    public boolean onOptionsItemSelectedAmin(MenuItem item) {
+        if (toggle.onOptionsItemSelected(item))
+
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
